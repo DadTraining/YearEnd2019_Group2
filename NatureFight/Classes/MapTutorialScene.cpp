5c26cb7eea7a7710fc67923f99ce2bef3629bb92
hdduy110697 <hdduy110697@gmail.com>
2019-12-17 13:50:41 +0700

    + attack,walking animation

--- NatureFight/Classes/MapTutorialScene.cpp
+++ NatureFight/Classes/MapTutorialScene.cpp
@@ -1,6 +1,9 @@
 #include "MapTutorialScene.h"
+#include <ObjectParent.h>
+#include <proj.win32\AiLv1.h>
 
 USING_NS_CC;
+using namespace std;
 
 Scene* MapTutorialScene::createScene()
 {
@@ -17,7 +20,13 @@ bool MapTutorialScene::init()
     auto visibleSize = Director::getInstance()->getVisibleSize();
     Vec2 origin = Director::getInstance()->getVisibleOrigin();
 	addMap();
+	AiLv1* aiLv1 = new AiLv1(this);
+	aiLv1->Init();
+	aiLv1->m_sprite->runAction(aiLv1->Attack());	
     return true;
+}
+void MapTutorialScene::update(FLOAT deltaTime)
+{
 }
 void MapTutorialScene::addMap()
 {
--- NatureFight/Classes/MapTutorialScene.h
+++ NatureFight/Classes/MapTutorialScene.h
@@ -1,15 +1,16 @@
 #ifndef __MAPTUTORIAL_SCENE_H__
 #define __MAPTUTORIAL_SCENE_H__
 
-#include "cocos2d.h"
-
+#include "cocos2d.h"#include "ObjectParent.h"
+#include "AiLv1.h"
 class MapTutorialScene : public cocos2d::Scene
 {
 public:
     static cocos2d::Scene* createScene();
 	void addMap();
     virtual bool init();
-    
+    void update(FLOAT deltaTime);
     CREATE_FUNC(MapTutorialScene);
+
 };
 #endif // __MAPTUTORIAL_SCENE_H__
