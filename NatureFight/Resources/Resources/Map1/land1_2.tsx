b152cd5c01dda7cb765e8833e8f1a51adb2bc3c2
XuanNhan <xuannhan2905@gmail.com>
2020-01-17 21:53:28 +0700

    End Gameloft inter

--- NatureFight/Resources/Resources/Map1/land1_2.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.1" name="land1-2" tilewidth="16" tileheight="16" tilecount="2048" columns="32">
- <image source="land__.png" width="512" height="1024"/>
-</tileset>
--- NatureFight/Resources/Resources/Map1/land_1.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.1" name="land_1" tilewidth="16" tileheight="16" tilecount="1890" columns="54">
- <image source="D0wHGpj.png" width="864" height="570"/>
-</tileset>
